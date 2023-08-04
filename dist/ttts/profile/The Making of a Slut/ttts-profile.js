469b6b72614126e28b0e79ab63f027dbb3cd1fc2
Elo-Ven <140505616+Elo-Ven@users.noreply.github.com>
2023-08-04 02:05:59 +0100

    exported profiles

---
+++ dist/ttts/profile/The Making of a Slut/ttts-profile.js
@@ -0,0 +1,29 @@
+/**
+ * TTTS - Twine Text To Speech
+ * Game Profile
+ *
+ * GAME DETAILS
+ * =========================
+ * NAME: The Making of a Slut
+ * VERSION: 0.9.4
+ *
+ * USAGE INSTRUCTIONS
+ * =========================
+ * Remove the // from the start of a line and change its value to enable the custom setting. For more information, please see the project website
+ *
+ * https://github.com/Elo-Ven/TTTS/
+ */
+
+var tttsProfile = {
+    //pitch: 1.5,
+    //rate: 1.5,
+    //voice: 0,
+    //volume: 0.5,
+    //
+    //container: '#example',
+    //debug: true,
+    //overwriteParams: ['trigger'],
+    silence: ['.flex-main-menu'],
+    //trigger: ['.example'],
+    //triggerIgnore: ['.example'],
+};

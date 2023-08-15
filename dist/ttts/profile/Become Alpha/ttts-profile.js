bb741717f36b4a0fc9e990c15d972dbf1854685d
Elo-Ven <140505616+Elo-Ven@users.noreply.github.com>
2023-08-15 13:47:43 +0100

    Become Alpha profile added

---
+++ dist/ttts/profile/Become Alpha/ttts-profile.js
@@ -0,0 +1,29 @@
+/**
+ * TTTS - Twine Text To Speech
+ * Game Profile
+ *
+ * GAME DETAILS
+ * =========================
+ * NAME: Become Alpha
+ * VERSION: 0.3.49
+ *
+ * USAGE INSTRUCTIONS
+ * =========================
+ * Remove the // from the start of a line and change its value to enable the custom setting. For more information, please see the project website
+ *
+ * https://github.com/Elo-Ven/TTTS/
+ */
+
+var tttsProfile = {
+	//pitch: 1.5,
+	//rate: 1.5,
+	//voice: 0,
+	//volume: 0.5,
+	//
+	container: "#passages .chunk.visible ,#passages .chunk.fiQ",
+	//debug: true,
+	//overwriteParams: ['trigger'],
+	silence: [".menu", ".help", ".btn"],
+	//trigger: ['.example'],
+	//triggerIgnore: ['.example'],
+};
